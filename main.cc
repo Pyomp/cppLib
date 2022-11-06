84446de553cf4e7b88871bd10537b061aa727e83
Nicolas Gayet <gayetni@gmail.com>
2022-11-06 18:16:55 +0100

    setup shared repo

--- main.cc
+++ main.cc
@@ -1,12 +1,9 @@
 #include <iostream>
 
-#include "hello.h"
-#include "SafetyThreadTest.h"
+// #include "SafetyThreadTest.h"
 #include "terminal.h"
 
-
 int main() {
-    std::cout << hello() << std::endl;
     // SafetyThreadTest::runTest();
     terminal::init(false);
     return 0;
--- utils/stringUtils.h
+++ utils/stringUtils.h
@@ -1,22 +1,8 @@
-#include <iostream>
-#include <sstream>
-#include <vector>
+#pragma once
 
-using namespace std;
+#include <vector>
+#include <string>
 
 namespace stringUtils {
-    vector<string> split(string s, string delimiter) {
-        size_t pos_start = 0, pos_end, delim_len = delimiter.length();
-        string token;
-        vector<string> res;
-
-        while ((pos_end = s.find(delimiter, pos_start)) != string::npos) {
-            token = s.substr(pos_start, pos_end - pos_start);
-            pos_start = pos_end + delim_len;
-            res.push_back(token);
-        }
-
-        res.push_back(s.substr(pos_start));
-        return res;
-    }
+    std::vector<std::string> split(std::string s, std::string delimiter);
 }
