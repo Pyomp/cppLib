0864e749206e2bf0da9c77c02c13924c5397ed43
Nicolas Gayet <gayetni@gmail.com>
2022-11-11 14:07:19 +0100

    server game co

--- utils/wsUtils.cc
+++ utils/wsUtils.cc
@@ -4,3 +4,7 @@ uint16_t wsUtils::getCommand(std::string message) {
     if (message.size() < 2) return 0;
     return (message[0] << 8) + message[1];
 };
+
+std::string wsUtils::getCommand(uint16_t command) {
+    return std::string((char*)&command, sizeof(uint16_t));
+};
--- utils/wsUtils.h
+++ utils/wsUtils.h
@@ -1,6 +1,9 @@
+#pragma once
+
 #include <cinttypes>
 #include <string>
 
 namespace wsUtils {
     uint16_t getCommand(std::string message);
+    std::string getCommand(uint16_t command);
 }
