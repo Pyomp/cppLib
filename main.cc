3fb900e12f465d714d46f4c29ed35f526fffd8f4
Nicolas Gayet <gayetni@gmail.com>
2022-11-15 23:23:29 +0100

    string buffer utils

--- main.cc
+++ main.cc
@@ -5,7 +5,7 @@
 // #include "structCast.h"
 // #include "EventSet.h"
 // #include "EventSetVoid.h"
-#include "stringBufferUtils.h"
+// #include "stringBufferUtils.h"
 #include <cstring>
 int main() {
     float a = 97.998798;
@@ -16,7 +16,7 @@ int main() {
     // std::cout << stringBufferUtils::getString(str, 4, 2) << std::endl;
     // std::cout << (int)stringBufferUtils::getUint8(str, 4) << std::endl;
     // std::cout << stringBufferUtils::getUint16(str, 2) << std::endl;
-    std::cout << stringBufferUtils::getFloat32(str, 0) << std::endl;
-    std::cout << a << std::endl;
+    // std::cout << stringBufferUtils::getFloat32(str, 0) << std::endl;
+    // std::cout << a << std::endl;
     return 0;
 }
--- utils/stringBufferUtils.cc
+++ utils/stringBufferUtils.cc
@@ -3,18 +3,23 @@
 
 #include "./stringBufferUtils.h"
 
+/* GETTERS */
+
 uint8_t stringBufferUtils::getUint8(std::string str, size_t offset) {
-    uint8_t a = str[offset];
+    uint8_t a;
+    std::memcpy(&a, &str[offset], sizeof(uint8_t));
     return a;
 };
 
 uint16_t stringBufferUtils::getUint16(std::string str, size_t offset) {
-    uint16_t a = (str[offset] << 8) | str[offset + 1];
+    uint16_t a;
+    std::memcpy(&a, &str[offset], sizeof(uint16_t));
     return a;
 }
 
 uint32_t stringBufferUtils::getUint32(std::string str, size_t offset) {
-    uint32_t a = (str[offset] << 24) | (str[offset + 1] << 16) | (str[offset + 2] << 8) | str[offset + 3];
+    uint32_t a;
+    std::memcpy(&a, &str[offset], sizeof(uint32_t));
     return a;
 }
 
@@ -27,3 +32,47 @@ float stringBufferUtils::getFloat32(std::string str, size_t  offset) {
 std::string stringBufferUtils::getString(std::string str, size_t offset, size_t size) {
     return std::string(&str[offset], size);
 };
+
+std::string stringBufferUtils::getString(uint8_t value) {
+    return std::string((char*)&value, sizeof(uint8_t));
+};
+
+std::string stringBufferUtils::getString(uint16_t value) {
+    return std::string((char*)&value, sizeof(uint16_t));
+};
+
+std::string stringBufferUtils::getString(uint32_t value) {
+    return std::string((char*)&value, sizeof(uint32_t));
+};
+
+std::string stringBufferUtils::getString(float value) {
+    return std::string((char*)&value, sizeof(float));
+};
+
+/* SETTERS */
+
+size_t setUint8(uint8_t value, std::string dest, size_t offset) {
+    std::memcpy(&dest[offset], &value, sizeof(uint8_t));
+    return sizeof(uint8_t);
+}
+
+size_t setUint16(uint16_t value, std::string dest, size_t offset) {
+    std::memcpy(&dest[offset], &value, sizeof(uint16_t));
+    return sizeof(uint16_t);
+}
+
+size_t setUint32(uint32_t value, std::string dest, size_t offset) {
+    std::memcpy(&dest[offset], &value, sizeof(uint32_t));
+    return sizeof(uint32_t);
+}
+
+size_t setFloat32(float value, std::string dest, size_t offset) {
+    std::memcpy(&dest[offset], &value, sizeof(float));
+    return sizeof(float);
+}
+
+size_t setString(std::string value, std::string dest, size_t offset) {
+    size_t size = value.size();
+    std::memcpy(&dest[offset], &value, size);
+    return size;
+}
