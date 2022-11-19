73777a273a357043abb582ff7a4c4742832a86bd
Nicolas Gayet <gayetni@gmail.com>
2022-11-19 10:22:48 +0100

    j

--- utils/wsUtils.h
+++ utils/wsUtils.h
@@ -5,7 +5,7 @@
 
 namespace wsUtils {
     uint16_t getCommand(std::string message);
-    std::string getCommand(uint16_t command);
+    // std::string getCommand(uint16_t command);
     uint16_t getId(std::string message);
-    std::string getId(uint16_t command);
+    // std::string getId(uint16_t command);
 }
