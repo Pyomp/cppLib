268bdf27a493890454d4e0982a9c929a99905cbe
n <gayetni@gmail.com>
2024-08-23 14:45:27 +0200

    oué

--- utils/StringBuffer.cc
+++ utils/StringBuffer.cc
@@ -2,68 +2,68 @@
 
 #include "StringBuffer.h"
 
-uint8_t StringBuffer::getUint8(const size_t offset) {
+uint8_t StringBuffer::getUint8(const size_t& offset) const {
     if (this->size() < sizeof(uint8_t) + offset) return 0;
     return *(uint8_t*)(this->_begin + offset);
 };
 
-uint16_t StringBuffer::getUint16(const size_t offset) {
+uint16_t StringBuffer::getUint16(const size_t& offset) const {
     if (this->size() < sizeof(uint16_t) + offset) return 0;
     return *(uint16_t*)(this->_begin + offset);
 }
 
-uint32_t StringBuffer::getUint32(const size_t offset) {
+uint32_t StringBuffer::getUint32(const size_t& offset)const {
     if (this->size() < sizeof(uint32_t) + offset) return 0;
     return *(uint32_t*)(this->_begin + offset);
 }
 
-float StringBuffer::getFloat32(const size_t  offset) {
+float StringBuffer::getFloat32(const size_t&  offset) const {
     if (this->size() < sizeof(float) + offset) return 0;
     return *(float*)(this->_begin + offset);
 }
 
-uint64_t StringBuffer::getUint64(const size_t offset) {
+uint64_t StringBuffer::getUint64(const size_t& offset)const {
     if (this->size() < sizeof(uint64_t) + offset) return 0;
     return *(uint64_t*)(this->_begin + offset);
 }
 
-size_t StringBuffer::setUint8(const uint8_t value, const size_t offset) {
+size_t StringBuffer::setUint8(const uint8_t& value, const size_t& offset) {
     if (this->size() < sizeof(uint8_t) + offset) return 0;
     std::memcpy(this->_begin + offset, &value, sizeof(uint8_t));
     return sizeof(uint8_t);
 }
 
-size_t StringBuffer::setUint16(const uint16_t value, const size_t offset) {
+size_t StringBuffer::setUint16(const uint16_t& value, const size_t& offset) {
     if (this->size() < sizeof(uint16_t) + offset) return 0;
     std::memcpy(this->_begin + offset, &value, sizeof(uint16_t));
     return sizeof(uint16_t);
 }
 
-size_t StringBuffer::setUint32(const uint32_t value, const size_t offset) {
+size_t StringBuffer::setUint32(const uint32_t& value, const size_t& offset) {
     if (this->size() < sizeof(uint32_t) + offset) return 0;
     std::memcpy(this->_begin + offset, &value, sizeof(uint32_t));
     return sizeof(uint32_t);
 }
 
-size_t StringBuffer::setUint64(const uint64_t value, const size_t offset) {
+size_t StringBuffer::setUint64(const uint64_t& value, const size_t& offset) {
     if (this->size() < sizeof(uint64_t) + offset) return 0;
     std::memcpy(this->_begin + offset, &value, sizeof(uint64_t));
     return sizeof(uint64_t);
 }
 
-size_t StringBuffer::setInt64(const int64_t value, const size_t offset) {
+size_t StringBuffer::setInt64(const int64_t& value, const size_t& offset) {
     if (this->size() < sizeof(int64_t) + offset) return 0;
     std::memcpy(this->_begin + offset, &value, sizeof(int64_t));
     return sizeof(int64_t);
 }
 
-size_t StringBuffer::setFloat32(const float value, const size_t offset) {
+size_t StringBuffer::setFloat32(const float &value, const size_t &offset) {
     if (this->size() < sizeof(value) + offset) return 0;
     std::memcpy(this->_begin + offset, &value, sizeof(float));
     return sizeof(float);
 }
 
-size_t StringBuffer::setString(const std::string value, const size_t offset) {
+size_t StringBuffer::setString(const std::string& value, const size_t& offset) {
     size_t size = value.size();
     if (this->size() < size + offset) return 0;
     std::memcpy(this->_begin + offset, &value, size);
--- utils/StringBuffer.h
+++ utils/StringBuffer.h
@@ -4,22 +4,22 @@
 
 class StringBuffer : public std::string {
 public:
-    StringBuffer(std::string buffer): std::string(buffer), _begin((void*)this->c_str()) {};
-    StringBuffer(const size_t size): std::string(size, '\0'), _begin((void*)this->c_str()) {};
+    StringBuffer(std::string buffer) : std::string(buffer), _begin((void*)this->c_str()) {};
+    StringBuffer(const size_t size) : std::string(size, '\0'), _begin((void*)this->c_str()) {};
 
-    uint8_t getUint8(const size_t offset);
-    uint16_t getUint16(const size_t offset);
-    uint32_t getUint32(const size_t offset);
-    uint64_t getUint64(const size_t offset);
-    float getFloat32(const size_t offset);
+    uint8_t getUint8(const size_t& offset) const;
+    uint16_t getUint16(const size_t& offset) const;
+    uint32_t getUint32(const size_t& offset) const;
+    uint64_t getUint64(const size_t& offset) const;
+    float getFloat32(const size_t& offset) const;
 
-    size_t setUint8(const uint8_t value, const size_t offset);
-    size_t setUint16(const uint16_t value, const size_t offset);
-    size_t setUint32(const uint32_t value, const size_t offset);
-    size_t setUint64(const uint64_t value, const size_t offset);
-    size_t setInt64(const int64_t value, const size_t offset);
-    size_t setFloat32(const float value, const size_t offset);
-    size_t setString(const std::string value, const size_t offset);
+    size_t setUint8(const uint8_t& value, const size_t& offset);
+    size_t setUint16(const uint16_t& value, const size_t& offset);
+    size_t setUint32(const uint32_t& value, const size_t& offset);
+    size_t setUint64(const uint64_t& value, const size_t& offset);
+    size_t setInt64(const int64_t& value, const size_t& offset);
+    size_t setFloat32(const float& value, const size_t& offset);
+    size_t setString(const std::string& value, const size_t& offset);
 private:
     void* _begin;
 };
